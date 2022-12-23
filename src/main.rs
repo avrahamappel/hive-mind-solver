23eadace16129df0a6373144e31186b80e142fb4
Avraham Appel <aappel248@gmail.com>
2022-12-22 20:10:15 -0500

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,23 @@ struct Board {
 }
 
 struct Player {
-    x: usize, y: usize,
+    x: usize,
+    y: usize,
 }
 
 enum Dir {
-    Up, Down, Right, Left }
+    Up,
+    Down,
+    Right,
+    Left,
+}
 
-enum State { Success(Vec<Dir>), Dead, Repeated, Just(Player) }
+enum State {
+    Success(Vec<Dir>),
+    Dead,
+    Repeated,
+    Just(Player),
+}
 
 enum Tile {
     None,
@@ -25,33 +35,36 @@ enum Tile {
 fn apply(d: Dir, b: Board, p: Player, visited: HashSet<Player>, history: Vec<Dir>) -> State {
     let new_p = match d {
         Dir::Up => Player { x: d.x, y: d.y - 1 },
-        Dir::Down => Player { x: d.x, y: d.y + 1},
+        Dir::Down => Player { x: d.x, y: d.y + 1 },
         Dir::Right => Player { x: d.x + 1, y: d.y },
         Dir::Left => Player { x: d.x - 1, y: d.y },
     };
 
     match b.getTile(new_p) {
         Tile::None => State::Just(new_p), // @TODO check if tile was already visited
-                                   Tile::Wall => State::Just(p),
-                                   Tile::Teleport => todo!(), // @TODO get position of other teleport
-                                                             Tile::Pit => State::Dead,
-                                                             Tile::Ice => apply(d, b, new_p), // Repeat this step (sliding along ice)
-                                                                                              Tile::Exit => State::Success(history),
+        Tile::Wall => State::Just(p),
+        Tile::Teleport => todo!(), // @TODO get position of other teleport
+        Tile::Pit => State::Dead,
+        Tile::Ice => apply(d, b, new_p), // Repeat this step (sliding along ice)
+        Tile::Exit => State::Success(history),
     }
 }
 
 /// Figure out how to get the player to the exit
-fn solve(b1: Board, p1: Player)->Option<Vec<Dir>> {
-    [Dir::Up, Dir::Down, Dir::Right, Dir::Left].iter().filterMap(|dir| {
-        let new_p = apply(dir, b1, p1);
+fn solve(b1: Board, p1: Player) -> Option<Vec<Dir>> {
+    [Dir::Up, Dir::Down, Dir::Right, Dir::Left]
+        .iter()
+        .filterMap(|dir| {
+            let new_p = apply(dir, b1, p1);
 
-        match new_p {
-            State::Success => Some(history),
-            State::Dead => None,
-            State::Repeated => None,
-            State::Just(np) => solve(b1, np),
-        }
-    }).first()
+            match new_p {
+                State::Success => Some(history),
+                State::Dead => None,
+                State::Repeated => None,
+                State::Just(np) => solve(b1, np),
+            }
+        })
+        .first()
 }
 
 fn main() {
